b843c034f802ab7a3ce9938a3a838e37677c4fd2
rajeev2834 <rajeev2834@gmail.com>
2022-02-02 22:18:29 +0530

    commit 02022022

---
+++ lib/models/vivad_type.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vivad_type.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+VivadType _$VivadTypeFromJson(Map<String, dynamic> json) {
+  return VivadType(
+    id: json['id'] as int,
+    vivad_type_hn: json['vivad_type_hn'] as String,
+  );
+}
+
+Map<String, dynamic> _$VivadTypeToJson(VivadType instance) => <String, dynamic>{
+      'id': instance.id,
+      'vivad_type_hn': instance.vivad_type_hn,
+    };
